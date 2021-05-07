5686d45f8b758e15090b84c97ed019956935a592
LuannMateus <luanmateuslima34@gmail.com>
2021-05-07 10:09:00 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,21 +9,21 @@ const Dashboard = () => {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Sales Dashboard</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success Percent (%)</h5>
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">All sales</h5>
+            <h5 className="text-center text-secondary">Todas vendas</h5>
             <DonutChart />
           </div>
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">All sales</h2>
+          <h2 className="text-primary">Todas vendas</h2>
         </div>
         <DataTable />
       </div>
